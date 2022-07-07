cc6fcbb5f43f3c2d54b051eb35da21b33510ea18
Amir Saleh Alavi <72743306+amirsaleh-alavi@users.noreply.github.com>
2022-07-08 02:15:48 +0430

    Singing in and up in progress

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
-import socket, threading
+import socket, threading, sys
 
-def handle_messages(connection: socket.socket):
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+
+def handle_messages(connection: socket.socket, username: str):
     '''
         Receive messages sent by the server and display them to user
     '''
@@ -13,7 +17,12 @@ def handle_messages(connection: socket.socket):
             # so the connection will be closed and an error will be displayed.
             # If not, it will try to decode message in order to show to user.
             if msg:
-                print(msg.decode())
+                if msg.decode() == "id":
+                    connection.send(username.encode())
+                if msg.decode().startswith("yourid"):
+                    username = connection.recv(1024).decode()[6:]
+                else:
+                    print(msg.decode())
             else:
                 connection.close()
                 break
@@ -29,17 +38,21 @@ def client() -> None:
         and handle it's input messages
     '''
 
-    SERVER_ADDRESS = '127.0.0.1'
-    SERVER_PORT = 12000
+    SERVER_ADDRESS = str(sys.argv[1])
+    SERVER_PORT = int(sys.argv[2])
 
     try:
+        username = "null"
+        receiver = "null"
         # Instantiate socket and start connection with server
         socket_instance = socket.socket()
         socket_instance.connect((SERVER_ADDRESS, SERVER_PORT))
         # Create a thread in order to handle messages sent by server
-        threading.Thread(target=handle_messages, args=[socket_instance]).start()
+        threading.Thread(target=handle_messages, args=[socket_instance, username]).start()
 
         print('Connected to chat!')
+        print('To send a message, enter "send"')
+        print('To exit the program, enter "quit"')
 
         # Read user's input until it quit from chat and close connection
         while True:
@@ -47,9 +60,13 @@ def client() -> None:
 
             if msg == 'quit':
                 break
+            if msg == 'send':
+                print("Who do you want to send your message to? (enter receivers's username)")
+                receiver = input()
 
+            message_to_send = username + ", " + receiver + ", " + msg
             # Parse message to utf-8
-            socket_instance.send(msg.encode())
+            socket_instance.send(message_to_send.encode())
 
         # Close connection with the server
         socket_instance.close()
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-import socket, threading
+import socket, threading, sys
+
+if len(sys.argv) != 2:
+    print("Correct usage: script, port number")
+    exit()
 
 # Global variable that mantain client's connections
 connections = []
@@ -18,10 +22,15 @@ def handle_user_connection(connection: socket.socket, address: str) -> None:
             if msg:
                 # Log message sent by user
                 print(f'{address[0]}:{address[1]} - {msg.decode()}')
+
+                parsed_message = msg.decode().split(",", 2)
+                sender = parsed_message[0]
+                receiver = parsed_message[1]
+                message = [2]
                 
                 # Build message format and broadcast to users connected on server
                 msg_to_send = f'From {address[0]}:{address[1]} - {msg.decode()}'
-                broadcast(msg_to_send, connection)
+                broadcast(message, connection, sender, receiver)
 
             # Close connection if no message was sent
             else:
@@ -34,7 +43,7 @@ def handle_user_connection(connection: socket.socket, address: str) -> None:
             break
 
 
-def broadcast(message: str, connection: socket.socket) -> None:
+def broadcast(message: str, connection: socket.socket, sender: str, receiver: str) -> None:
     '''
         Broadcast message to all users connected to the server
     '''
@@ -44,8 +53,11 @@ def broadcast(message: str, connection: socket.socket) -> None:
         # Check if isn't the connection of who's send
         if client_conn != connection:
             try:
-                # Sending message to client connection
-                client_conn.send(message.encode())
+                client_conn.send("id".encode())
+                received_id = client_conn.recv(1024)
+                if received_id.decode == receiver:
+                    # Sending message to client connection
+                    client_conn.send(message.encode())
 
             # if it fails, there is a chance of socket has died
             except Exception as e:
@@ -71,13 +83,13 @@ def server() -> None:
         to handle their messages
     '''
 
-    LISTENING_PORT = 12000
+    LISTENING_PORT = int(sys.argv[1])
     
     try:
         # Create server and specifying that it can only handle 4 connections by time!
         socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_instance.bind(('', LISTENING_PORT))
-        socket_instance.listen(4)
+        socket_instance.listen(100)
 
         print('Server running!')
         
@@ -85,6 +97,97 @@ def server() -> None:
 
             # Accept client connection
             socket_connection, address = socket_instance.accept()
+
+            def SignInUp():
+                while True:
+                    socket_connection.send("SignIn (I), or SighUp (U)?".encode())
+                    answer = socket_connection.recv(16).decode()
+                    answer = answer[12:]
+                    if answer in ("U", "I"):
+                        return answer
+                        break
+                    else:
+                        continue
+
+            operation = SignInUp()
+
+            userslist_path = "/root/db/userslist.txt"
+            userslist_operation = open(userslist_path, "r+")
+            userslist = [line for line in userslist_operation.readlines()]
+
+            def SignUp():
+                socket_connection.send("Enter a username of your choice:".encode())
+                requesteduserid = (socket_connection.recv(32)).decode()
+                requesteduserid = requesteduserid[12:]
+                if not requesteduserid in userslist:
+                    socket_connection.send("You may use this username.".encode())
+                    socket_connection.send("Enter your desired password:".encode())
+                    requestedpassword = (socket_connection.recv(32)).decode()
+                    requestedpassword = requestedpassword[12:]
+                    
+                    userslistpath = "/root/db/userslist.txt"
+                    f = open(userslistpath,"a+")
+                    f.write(requesteduserid + "\n")
+                    f.close()
+
+                    usernamepath = "/root/db/usersinfo/"+requesteduserid+".username.txt"
+                    f = open(usernamepath,"a+")
+                    f.write(requesteduserid)
+                    f.close()
+
+                    passpath = "/root/db/usersinfo/"+requesteduserid+".pass.txt"
+                    f = open(passpath,"a+")
+                    f.write(requestedpassword)
+                    f.close()
+
+                    socket_connection.send("You have been registered successfully. Now please log in.".encode())
+                    return True
+                else:
+                    socket_connection.send("This username is taken, try another one.".encode())
+                    return False
+
+            def SignIn():
+                socket_connection.send("Enter your username:".encode())
+                username = (socket_connection.recv(32)).decode()
+                username = username[12:]
+                socket_connection.send("Enter your passowrd:".encode())
+                password = (socket_connection.recv(32)).decode()
+                password = password[12:]
+
+                if username+"\n" in userslist:
+                    correctpasspath = "/root/db/usersinfo/"+username+".pass.txt"
+                    with open(correctpasspath, 'r') as key:
+                        correctpass = key.read().rstrip()
+
+                    if password == correctpass:
+                        socket_connection.send("Logged in successfully!".encode())
+                        yourid = "yourid" + username
+                        socket_connection.send(yourid.encode())
+                        return True
+                    else:
+                        socket_connection.send("Username or Password in incorrect. Try again!".encode())
+                        return False
+                else:
+                    socket_connection.send("Username or Password in incorrect. Try again!".encode())
+                    return False
+
+
+            if operation == "U":
+                while True:
+                    signupsuccess = SignUp()
+                    if signupsuccess == True:
+                        signinsuccess = SignIn()
+                        if signinsuccess == True:
+                            break
+            elif operation == "I":
+                while True:
+                    signinsuccess = SignIn()
+                    if signinsuccess == True:
+                        break
+
+
+
+
             # Add client connection to connections list
             connections.append(socket_connection)
             # Start a new thread to handle client connection and receive it's messages
